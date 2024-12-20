7b2febd6e69c393ac0f907ea44c3c2a17b0dd713
Saturnityx <67051812+Saturnityx@users.noreply.github.com>
2024-12-20 14:46:13 +0200

    Add files via upload
    2. Parim aja jutumus on O(n) ja halvim juht on O(n*m)
    3. Päris elus kasutatakse indekseerimist andmebaasides

---
+++ ül2.py
@@ -0,0 +1,22 @@
+def index_mapping(keys, table_size):
+    # Loo massiiv vastava suurusega
+    hash_table = [None] * table_size
+    
+    for key in keys:
+        # Kasutame triviaalset kaardistamist (nt võtme väärtus mod tabeli suurus)
+        index = key % table_size
+        
+        # Kui koht on juba hõivatud, liigu järgmisele vaba kohale (naivne lähenemine)
+        while hash_table[index] is not None:
+            index = (index + 1) % table_size
+        
+        # Salvestame väärtuse vastavasse kohta
+        hash_table[index] = key
+    
+    return hash_table
+
+# Näide kasutamisest:
+keys = [10, 20, 30, 40, 50]
+table_size = 7
+hash_table = index_mapping(keys, table_size)
+print(hash_table)
