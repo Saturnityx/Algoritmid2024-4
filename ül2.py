80156e7bf89073bfdbc350affa9237f1b093e698
Saturnityx <67051812+Saturnityx@users.noreply.github.com>
2024-12-20 14:47:15 +0200

    Update ül2.py

--- ül2.py
+++ ül2.py
@@ -20,3 +20,6 @@ keys = [10, 20, 30, 40, 50]
 table_size = 7
 hash_table = index_mapping(keys, table_size)
 print(hash_table)
+
+# 2. Parim aja jutumus on O(n) ja halvim juht on O(n*m)
+# 3. Päris elus kasutatakse indekseerimist andmebaasides
